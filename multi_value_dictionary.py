8fd9114ce4b0eca286c80233649de86e3e44f04f
Eric Lee <hownl96@gmail.com>
2025-02-04 22:28:02 -0600

    feat: create boilerplate code and empty functions

---
+++ multi_value_dictionary.py
@@ -0,0 +1,71 @@
+### KEYS
+# Returns all the keys in the dictionary.
+# Order is not guaranteed.
+def keys():
+    return
+
+### MEMBERS
+# Returns the collection of strings for the given key. 
+# Return order is not guaranteed.
+# Returns an error if the key does not exists.
+def members(key):
+    if False:
+        return "ERROR, key does not exist."
+    return
+
+### ADD
+# Adds a member to a collection for a given key.
+# Displays an error if the member already exists for the key.
+def add(key, value):
+    if key_exists():
+        return
+    return
+
+### REMOVE
+# Removes a member from a key.
+# If the last member is removed from the key, the key is removed from the dictionary.
+# If the key or member does not exist, displays an error.
+def remove():
+    return
+
+### REMOVEALL
+# Removes all members for a key and removes the key from the dictionary.
+# Returns an error if the key does not exist.
+def remove_all():
+    return
+
+### CLEAR
+# Removes all keys and all members from the dictionary.
+def clear():
+    return
+
+### KEYEXISTS
+# Returns whether a key exists or not.
+def key_exists():
+    return True
+
+### MEMBEREXISTS
+# Returns whether a member exists within a key.
+# Returns false if the key does not exist.
+def member_exists():
+    return
+
+### ALLMEMBERS
+# Returns all the members in the dictionary.
+# Returns nothing if there are none. Order is not guaranteed.
+def all_members();
+    return
+
+### ITEMS
+# Returns all keys in the dictionary and all of their members.
+# Returns nothing if there are none.
+# Order is not guaranteed.
+def items():
+    return
+
+
+def main():
+    dictionary = {}
+
+    # write out loop for application in order to receive argument inputs
+    return
